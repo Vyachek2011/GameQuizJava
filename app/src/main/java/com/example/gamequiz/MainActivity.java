43c8248cd421c899ffd1079025c9439842318cc4
GNote <Slava_Gudkov@mail.ru>
2020-07-20 00:45:40 +0300

    Новый проект игры "Викторина"

--- app/src/main/java/com/example/gamequiz/MainActivity.java
+++ app/src/main/java/com/example/gamequiz/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    //Новый проект проверка выгрузки
+    //Новый проект проверка выгрузки апаа
 
 }
