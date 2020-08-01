914fa1b2d3e488da3bb44b8de506d6fd4fe9724d
GNote <Slava_Gudkov@mail.ru>
2020-08-01 17:39:02 +0300

    Сделана глвная страница игры и обработка закрытия игры

--- app/src/main/java/com/example/gamequiz/MainActivity.java
+++ app/src/main/java/com/example/gamequiz/MainActivity.java
@@ -3,15 +3,36 @@ package com.example.gamequiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    long TimeBackPress;
+    Toast MyToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
 
+    @Override
+    public void onBackPressed() {
+        if (TimeBackPress + 2000 > System.currentTimeMillis()){
+            super.onBackPressed();
+            MyToast.cancel();
+            return;
+        }
+        else{
+            MyToast = Toast.makeText(getBaseContext(), "Нажмите еще раз, чтобы выйти", Toast.LENGTH_SHORT);
+            MyToast.show();
+        }
 
-
+        TimeBackPress = System.currentTimeMillis();
+    }
 }
