02e0db7eb26840cff17a6bbdb1c1012eb468f316
GNote <Slava_Gudkov@mail.ru>
2020-08-02 18:10:38 +0300

    Добавлена страница универсального уровня

--- app/src/main/java/com/example/gamequiz/GameLevel.java
+++ app/src/main/java/com/example/gamequiz/GameLevel.java
@@ -29,6 +29,5 @@ public class GameLevel extends AppCompatActivity {
             }
         });
 
-
     }
 }
---
+++ app/src/main/java/com/example/gamequiz/Level1.java
@@ -0,0 +1,22 @@
+package com.example.gamequiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class Level1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.universal);
+
+        Window window = getWindow();
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+    }
+}
