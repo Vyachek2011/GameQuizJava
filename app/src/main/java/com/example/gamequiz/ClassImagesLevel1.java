33f09fd9a6704ea8a28097557781752e305392bf
GNote <Slava_Gudkov@mail.ru>
2020-08-08 16:07:49 +0300

    Сделана загрузка картинок и их анализ. Сделана передача номера выбранного уровня.

---
+++ app/src/main/java/com/example/gamequiz/ClassImagesLevel1.java
@@ -0,0 +1,19 @@
+package com.example.gamequiz;
+
+public class ClassImagesLevel1{
+
+    final int[] ListImages = {R.drawable.level1_number0,
+                              R.drawable.level1_number1,
+                              R.drawable.level1_number2,
+                              R.drawable.level1_number3,
+                              R.drawable.level1_number4,
+                              R.drawable.level1_number5,
+                              R.drawable.level1_number6,
+                              R.drawable.level1_number7,
+                              R.drawable.level1_number8,
+                              R.drawable.level1_number9};
+
+    //final String[] StringNumbers = getResources().getStringArray(R.array.ValueNumbers);
+
+    final int[] ListNumber = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+}
